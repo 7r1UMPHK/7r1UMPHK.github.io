f19f01280fbfa5a1beb4c6a721cc8f0762631fdf
7r1UMPHK <217751621+7r1UMPHK@users.noreply.github.com>
2026-06-26 19:33:34 +0800

    Update FriendLinks.js

--- static/plugins/FriendLinks.js
+++ static/plugins/FriendLinks.js
@@ -175,7 +175,7 @@ document.addEventListener('DOMContentLoaded', function() {
             },
             {
                 name: 'the0n3/Apursuit',
-                avatar: 'https://the0n3.top/img/favicon.ico',
+                avatar: 'https://the0n3.top/favicon.ico',
                 url: 'https://the0n3.top',
                 description: "npc",
                 type: 'blog'
